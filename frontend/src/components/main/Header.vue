d732caa17d9b66a50ad7cec17749ecd6d950139a
kiwon <kiwon0502@gmail.com>
2020-12-05 23:05:14 +0900

    헤더수정완료 /CSV 파일 다운로드 기능 필요

--- frontend/src/components/main/Header.vue
+++ frontend/src/components/main/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">Home</b-navbar-brand>
+      <b-navbar-brand href="#"><img src="../../assets/EDs_DBD.png" alt="logo"></b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
